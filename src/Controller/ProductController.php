9cc7de519abb615e66a8cb50c1f82d11bcc46ca3
Bastien <bastien.biblocque@gmail.com>
2024-08-12 12:17:22 +0200

    :sparkles: Crud produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -18,29 +18,108 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'product_index', methods: ['GET'])]
-    public function index(int $shopId, ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    public function index(int $shopId, ProductRepository $productRepository): JsonResponse
     {
         $products = $productRepository->findBy(['shopId' => $shopId]);
         return $this->json($products, Response::HTTP_OK);
     }
 
+    #[Route('/{productId}', name: 'get_product', methods: ['GET'])]
+    public function getProductById(int $productId, ProductRepository $productRepository): JsonResponse
+    {
+        $product = $productRepository->find($productId);
+
+        if (!$product) {
+            return new JsonResponse(['error' => 'Product not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($product, Response::HTTP_OK);
+    }
+
     #[Route('/new', name: 'product_new', methods: ['POST'])]
-    public function new(int $shopId, ShopRepository $shopRepository, Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    public function new(int $shopId, ShopRepository $shopRepository, Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         $product = new Product();
         $product->setName($data['name']);
         $product->setPrice($data['price']);
         $product->setQuantity($data['quantity']);
-        $product->setDescription($data['description'] ?? null);
+        $product->setDescription($data['description'] ?? 'description');
         $shop = $shopRepository->find($shopId);
-
-        $product->setShopId($shop);
+        $product->setShopId($shop->getId());
 
         $entityManager->persist($product);
         $entityManager->flush();
 
         return $this->json($product, Response::HTTP_OK);
+    }
+
+    #[Route('/{productId}', name: 'update_product', methods: ['PUT'])]
+    public function updateProduct(Request $request, int $shopId, int $productId, ShopRepository $shopRepository, ProductRepository $productRepository, EntityManagerInterface $entityManager): JsonResponse
+    {
+        // Trouver le shop par ID
+        $shop = $shopRepository->find($shopId);
+
+        if (!$shop) {
+            return new JsonResponse(['error' => 'Shop not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $product = $productRepository->find($productId);
+
+        if (!$product || $product->getShopId() !== $shop->getId()) {
+            return new JsonResponse(['error' => 'Product not found or does not belong to the shop'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['name'])) {
+            $product->setName($data['name']);
+        }
+        if (isset($data['description'])) {
+            $product->setDescription($data['description']);
+        }
+        if (isset($data['price'])) {
+            $product->setPrice($data['price']);
+        }
+        if (isset($data['quantity'])) {
+            $product->setQuantity($data['quantity']);
+        }
+
+        try {
+            $entityManager->persist($product);
+            $entityManager->flush();
+            return $this->json($product, Response::HTTP_OK);
+
+            return new JsonResponse(['message' => 'Product updated successfully'], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'An error occurred while updating the product'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    #[Route('/{productId}', name: 'delete_product', methods: ['DELETE'])]
+    public function deleteProduct(int $shopId, int $productId, EntityManagerInterface $entityManager, ShopRepository $shopRepository, ProductRepository $productRepository): JsonResponse
+    {
+        // Trouver le shop par ID
+        $shop = $shopRepository->find($shopId);
+
+        if (!$shop) {
+            return new JsonResponse(['error' => 'Shop not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $product = $productRepository->find($productId);
+
+        if (!$product || $product->getShopId() !== $shop->getId()) {
+            return new JsonResponse(['error' => 'Product not found or does not belong to the shop'], Response::HTTP_NOT_FOUND);
+        }
+
+        // Suppression du produit
+        try {
+            $entityManager->remove($product);
+            $entityManager->flush();
+            return new JsonResponse(['message' => 'Product deleted successfully'], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'An error occurred while deleting the product'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
