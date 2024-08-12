60e188fb6f50f2f8a1ea821784146e16aa699a28
Bastien <bastien.biblocque@gmail.com>
2024-08-12 11:41:39 +0200

    Identification

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -16,10 +16,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api/user')]
-class UserController extends AbstractController
+#[Route('/api/private/{shopId}/customer')]
+class CustomerController extends AbstractController
 {
-    #[Route('/new', name: 'user_new', methods: ['POST'])]
+    #[Route('/new', name: 'customer_new', methods: ['POST'])]
     public function new(Request $request,EntityManagerInterface $entityManager ,UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, ValidatorInterface $validator, SerializerInterface $serializer): JsonResponse
     {
         $data = $request->getContent();
